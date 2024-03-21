9b85ad0bff9a53126f7c040ca8014ce0fdccbac0
KIRAN SATDIVE <98806225+kiransatdive@users.noreply.github.com>
2024-03-22 00:33:40 +0530

    Update app_constant.dart

--- lib/widget/app_constant.dart
+++ lib/widget/app_constant.dart
@@ -1,5 +1,5 @@
 String publishableKey =
-    "pk_test_51OhYVWSBb7nVsXDjxhV2W3zMswpnJmpXN4AvQQdaJuLrWvCFIvLv8FxmY6r3CLOrpjZ5TAhb1eXXuWANNW6I2dfQ00GSKK1AU9";
+    "";
 
 String secretKey =
-    "sk_test_51OhYVWSBb7nVsXDjjamYSIBNJzHcyLA6kQkeo4z6zePlVrtqj0pamDB4uYlEsvMHCvUpKHACeGLqfYQR0p338a4S00mQpPmMk2";
+    "";
